0cbecd0abd237f045be0c65b07032cec34c829f4
jordi <jsabat@gmail.com>
2020-06-26 15:42:49 +0100

    Added auth and front page UI

---
+++ resources/views/home.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <col-3 class="p-5">
+            <img src="https://instagram.flis5-1.fna.fbcdn.net/v/t51.2885-19/s150x150/23161150_1372068142919939_4240897999955296256_n.jpg?_nc_ht=instagram.flis5-1.fna.fbcdn.net&_nc_ohc=kpBwQe5BmJ8AX_3yEg_&oh=d27af356ec80218fd28f55bbac616a20&oe=5F2021E7" alt="" class="rounded-circle">
+        </col-3>
+        <col-9 class="pt-5">
+            <div>
+                <h1>followbirdsapp</h1>
+            </div>
+            <div class="d-flex">
+                <div class="pr-4"><strong>102</strong> posts</div>
+                <div class="pr-4"><strong>264</strong> followers</div>
+                <div><strong>228</strong> following</div>
+            </div>
+            <div class="pt-4 font-weight-bold">
+                Followbirds
+            </div>
+            <div>
+                Designed for those who love birds and enjoy the outdoors, Followbirds provides the tools you need to take full advantage of your trip.
+            </div>
+            <div><a href="https://www.followbirds.com">www.followbirds.com</a></div>
+        </col-9>
+    </div>
+
+    <div class="row pt-5">
+        <div class="col-4"><img src="https://instagram.flis5-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/s640x640/100087142_282472716124836_6279186573306542377_n.jpg?_nc_ht=instagram.flis5-1.fna.fbcdn.net&_nc_cat=100&_nc_ohc=OIqETTT1x2cAX-HJSzO&oh=6b814543b1e923deb272bb771a2c792f&oe=5EF85731" alt="" class="w-100"></div>
+        <div class="col-4"><img src="https://instagram.flis5-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/s640x640/100359181_142925604008507_6306212278405059032_n.jpg?_nc_ht=instagram.flis5-1.fna.fbcdn.net&_nc_cat=111&_nc_ohc=IO1od_o0V-IAX-8WmhZ&oh=61c749eb23fa8b46ce1d33d908a84ba6&oe=5EF82892" alt="" class="w-100"></div>
+        <div class="col-4"><img src="https://instagram.flis5-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/s640x640/94226596_1172791323055143_5187791639198071543_n.jpg?_nc_ht=instagram.flis5-1.fna.fbcdn.net&_nc_cat=103&_nc_ohc=L4Sf5bs-GKEAX8R0O-A&oh=bd46d6cd4c968e68eb47ffd62a8d104c&oe=5F1FB7D6" alt="" class="w-100"></div>
+    </div>
+    <div class="row pt-5">
+        <div class="col-4"><img src="https://instagram.flis5-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/s640x640/83078108_1547400338749935_6525845539417778467_n.jpg?_nc_ht=instagram.flis5-1.fna.fbcdn.net&_nc_cat=103&_nc_ohc=fK1OJjdpSmEAX-dr4Im&oh=1b645774273c424fdad427c9c2d73d08&oe=5F2174CF" alt="" class="w-100"></div>
+        <div class="col-4"><img src="https://instagram.flis5-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/s640x640/92572184_226059558477037_1163050933183122448_n.jpg?_nc_ht=instagram.flis5-1.fna.fbcdn.net&_nc_cat=101&_nc_ohc=id_0h4Bmu9AAX-Q7D90&oh=dce9e636128d18ac8d88f9b4e6065a99&oe=5EF81E60" alt="" class="w-100"></div>
+        <div class="col-4"><img src="https://instagram.flis5-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/s640x640/90900666_2333697840261905_1802397198490102273_n.jpg?_nc_ht=instagram.flis5-1.fna.fbcdn.net&_nc_cat=105&_nc_ohc=HY4R7eo4UYgAX_6aUPM&oh=b72769addff0e2d758fffb1dc781df80&oe=5F1F89BB" alt="" class="w-100"></div>
+    </div>
+</div>
+@endsection
